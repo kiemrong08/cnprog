2a0ee0e210076c11e58df5241b08319e1b817701
chagel <chagel@b6dd08b8-cd4e-11dd-94fd-9de6169c3690>
2008-12-25 14:22:32 +0000

    添加需要积分进行功能的权限判断和模板filter

---
+++ forum/auth.py
@@ -0,0 +1,91 @@
+﻿"""
+Authorisation related functions.
+
+The actions a User is authorised to perform are dependent on their reputation
+and superuser status.
+"""
+VOTE_UP                   = 15
+FLAG_OFFENSIVE            = 15
+POST_IMAGES               = 15
+LEAVE_COMMENTS            = 50
+VOTE_DOWN                 = 100
+CLOSE_OWN_QUESTIONS       = 250
+RETAG_OTHER_QUESTIONS     = 500
+EDIT_COMMUNITY_WIKI_POSTS = 750
+EDIT_OTHER_POSTS          = 2000
+DELETE_COMMENTS           = 2000
+VIEW_OFFENSIVE_FLAGS      = 2000
+DISABLE_URL_NOFOLLOW      = 2000
+CLOSE_OTHER_QUESTIONS     = 3000
+LOCK_POSTS                = 4000
+
+def can_vote_up(user):
+    """Determines if a User can vote Questions and Answers up."""
+    return user.is_authenticated() and (
+        user.reputation >= VOTE_UP or
+        user.is_superuser)
+
+def can_flag_offensive(user):
+    """Determines if a User can flag Questions and Answers as offensive."""
+    return user.is_authenticated() and (
+        user.reputation >= FLAG_OFFENSIVE or
+        user.is_superuser)
+
+def can_add_comments(user):
+    """Determines if a User can add comments to Questions and Answers."""
+    return user.is_authenticated() and (
+        user.reputation >= LEAVE_COMMENTS or
+        user.is_superuser)
+
+def can_vote_down(user):
+    """Determines if a User can vote Questions and Answers down."""
+    return user.is_authenticated() and (
+        user.reputation >= VOTE_DOWN or
+        user.is_superuser)
+
+def can_retag_questions(user):
+    """Determines if a User can retag Questions."""
+    return user.is_authenticated() and (
+        RETAG_OTHER_QUESTIONS <= user.reputation < EDIT_OTHER_POSTS)
+
+def can_edit_post(user, post):
+    """Determines if a User can edit the given Question or Answer."""
+    return user.is_authenticated() and (
+        user.id == post.author_id or
+        (post.wiki and user.reputation >= EDIT_COMMUNITY_WIKI_POSTS) or
+        user.reputation >= EDIT_OTHER_POSTS or
+        user.is_superuser)
+
+def can_delete_comment(user, comment):
+    """Determines if a User can delete the given Comment."""
+    return user.is_authenticated() and (
+        user.id == comment.user_id or
+        user.reputation >= DELETE_COMMENTS or
+        user.is_superuser)
+
+def can_view_offensive_flags(user):
+    """Determines if a User can view offensive flag counts."""
+    return user.is_authenticated() and (
+        user.reputation >= VIEW_OFFENSIVE_FLAGS or
+        user.is_superuser)
+
+def can_close_question(user, question):
+    """Determines if a User can close the given Question."""
+    return user.is_authenticated() and (
+        (user.id == question.author_id and
+         user.reputation >= CLOSE_OWN_QUESTIONS) or
+        user.reputation >= CLOSE_OTHER_QUESTIONS or
+        user.is_superuser)
+
+def can_lock_posts(user):
+    """Determines if a User can lock Questions or Answers."""
+    return user.is_authenticated() and (
+        user.reputation >= LOCK_POSTS or
+        user.is_superuser)
+
+def can_follow_url(user):
+    """Determines if the URL link can be followed by Google search engine."""
+    return user.is_authenticated() and (
+        user.reputation >= DISABLE_URL_NOFOLLOW or
+        user.is_superuser
+    )
---
+++ forum/templatetags/extra_filters.py
@@ -0,0 +1,44 @@
+﻿from django import template
+from forum import auth
+
+register = template.Library()
+
+@register.filter
+def can_vote_up(user):
+    return auth.can_vote_up(user)
+
+@register.filter
+def can_flag_offensive(user):
+    return auth.can_flag_offensive(user)
+
+@register.filter
+def can_add_comments(user):
+    return auth.can_add_comments(user)
+
+@register.filter
+def can_vote_down(user):
+    return auth.can_vote_down(user)
+
+@register.filter
+def can_retag_questions(user):
+    return auth.can_retag_questions(user)
+
+@register.filter
+def can_edit_post(user, post):
+    return auth.can_edit_post(user, post)
+
+@register.filter
+def can_delete_comment(user, comment):
+    return auth.can_delete_comment(user, comment)
+
+@register.filter
+def can_view_offensive_flags(user):
+    return auth.can_view_offensive_flags(user)
+
+@register.filter
+def can_close_question(user, question):
+    return auth.can_close_question(user, question)
+
+@register.filter
+def can_lock_posts(user):
+    return auth.can_lock_posts(user)
